abfa4395a22287233e4d299139d298a027e33d60
Pritam Sinha <Pritam Sinha@DESKTOP-4M9TNSB>
2020-05-10 19:58:19 +0530

    fixed issue

--- deploy/src/main/java/com/test/deploy/App.java
+++ deploy/src/main/java/com/test/deploy/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Deploy successfully!" );
+    	String name="john";
+        System.out.println( "Deploy successfully! "+name );
     }
 }
