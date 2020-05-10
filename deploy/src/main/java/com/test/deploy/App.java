f61a5ca4d679d576ec6a2779fbae60847e8c9d85
Pritam Sinha <Pritam Sinha@DESKTOP-4M9TNSB>
2020-05-10 15:54:12 +0530

    deploy

---
+++ deploy/src/main/java/com/test/deploy/App.java
@@ -0,0 +1,13 @@
+package com.test.deploy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Deploy successfully!" );
+    }
+}
